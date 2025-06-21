61eac12d88b1eb3ca0bb03c31c1b6fe838022631
SnanCakmak <84072765+SnanCakmak@users.noreply.github.com>
2025-06-21 18:45:38 +0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@
 // Giriş ekranı şifre kontrolü
 function kontrolEt() {
   const password = document.getElementById("sifre").value;
-  if (password === "sirius2025") {
+  if (password === "sinan") {
     window.location.href = "dashboard.html";
   } else {
     alert("Hatalı şifre!");
